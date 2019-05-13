8890ca7dbe7afe35f1a39c27cf7c4e0f31485a1b
italo alcantara <italo.ads31@gmail.com>
2019-05-13 06:37:14 -0300

    Creation RequestParam and test

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,13 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
 public class IndexController {
 	@GetMapping(value = "/",produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá REST Spring",HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value="nome",required = false) String nome){
+		System.out.println("Parametro" + nome);
+		return new ResponseEntity("Olá REST Spring parametro "+nome ,HttpStatus.OK);
 	}
 }
